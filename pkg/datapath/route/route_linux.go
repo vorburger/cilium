4b2ff4591656a036d011180b4ae1d4b3db874440
John Fastabend <john.fastabend@gmail.com>
2019-01-17 18:44:09 +0000

    cilium: ipsec, extend datapath route pkg to support more specifiers
    In order to support IPSec we use specific routes inserted into a
    table designated for IPSec. To further isolate these entries we
    also constrain the routes using the proto (proto=50 for ESP) field
    and insert as a "local" route to ensure traffic is processed by the
    IPSec in kernel code on the correct device. Details on IPSec
    implementation in documentation and subsequent patches.
    
    To facilitate this add support in route pkg to program these from
    the API.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- pkg/datapath/route/route_linux.go
+++ pkg/datapath/route/route_linux.go
@@ -29,9 +29,12 @@ import (
 // getNetlinkRoute returns the route configuration as netlink.Route
 func (r *Route) getNetlinkRoute() netlink.Route {
 	rt := netlink.Route{
-		Dst: &r.Prefix,
-		Src: r.Local,
-		MTU: r.MTU,
+		Dst:      &r.Prefix,
+		Src:      r.Local,
+		MTU:      r.MTU,
+		Protocol: r.Proto,
+		Table:    r.Table,
+		Type:     r.Type,
 	}
 
 	if r.Nexthop != nil {
@@ -40,6 +43,8 @@ func (r *Route) getNetlinkRoute() netlink.Route {
 
 	if r.Scope != 0 {
 		rt.Scope = r.Scope
+	} else if r.Scope == 0 && r.Type == 2 {
+		rt.Scope = netlink.SCOPE_HOST
 	}
 
 	return rt
@@ -101,6 +106,10 @@ func lookup(link netlink.Link, route *netlink.Route) *netlink.Route {
 		}
 
 		if route.Dst != nil && r.Dst == nil {
+			continue
+		}
+
+		if route.Table != r.Table {
 			continue
 		}
 
